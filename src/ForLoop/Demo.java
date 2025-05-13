5f89c3a31c02025646d3a6251d59485740b0ef24
anikethukire922001@gmail.com <anikethukire922001@gmail.com>
2025-05-13 22:23:14 +0530

    frist commit

---
+++ src/ForLoop/Demo.java
@@ -0,0 +1,12 @@
+package ForLoop;
+
+public class Demo {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1; i<=10; i++) {
+			sum=sum+i;
+		}
+		System.out.println("The sum of frist 10 natual number bers is"+ sum);
+	}
+
+}
